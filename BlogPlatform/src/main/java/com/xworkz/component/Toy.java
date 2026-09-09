b7eb1afc3410d113b7eabd18df7e9cc0c1751b6f
Akasha777 <akashmlk2021@gmail.com>
2026-09-09 23:52:59 +0530

    Bean annotation in Spring

--- BlogPlatform/src/main/java/com/xworkz/component/Toy.java
+++ BlogPlatform/src/main/java/com/xworkz/component/Toy.java
@@ -1,11 +1,19 @@
 package com.xworkz.component;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Toy {
     public Toy(){
         System.out.println("The Toy created");
+    }
+
+    @Bean
+    public Toy getToy(){
+        Toy toy = new Toy();
+        System.out.println("The toy is not created by Bean");
+        return toy;
     }
 }
 
