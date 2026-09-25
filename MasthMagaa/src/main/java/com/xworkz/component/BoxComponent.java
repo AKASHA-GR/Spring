0e3eb98183ffebf14aa367c7f985a2dbd3579720
Akasha777 <akashmlk2021@gmail.com>
2026-09-25 23:47:18 +0530

    spring task

---
+++ MasthMagaa/src/main/java/com/xworkz/component/BoxComponent.java
@@ -0,0 +1,11 @@
+package com.xworkz.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BoxComponent {
+
+    public BoxComponent(){
+        System.out.println("The Box component is created.");
+    }
+}
